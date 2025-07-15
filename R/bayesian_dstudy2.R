6153a8a29da756eb7805559d47615c1f8bf5cd6a
t-clark04 <t_clark04@yahoo.com>
2025-07-15 10:15:31 -0400

    Fixing functions to account for funky column headings

--- R/bayesian_dstudy2.R
+++ R/bayesian_dstudy2.R
@@ -67,7 +67,7 @@ bayesian_dstudy2 <- function(data, col.scores, col.subjects, col.facet1, col.fac
   })
 
   # Setting the formula and running the brms model according to the user's specifications.
-  formula1 <- glue::glue("{col.scores} ~ (1|{col.subjects}) + (1|{col.facet1}) + (1|{col.facet2}) + (1|{col.subjects}:{col.facet1}) + (1|{col.subjects}:{col.facet2}) + (1|{col.facet1}:{col.facet2})")
+  formula1 <- glue::glue("`{col.scores}` ~ (1|`{col.subjects}`) + (1|`{col.facet1}`) + (1|`{col.facet2}`) + (1|`{col.subjects}`:`{col.facet1}`) + (1|`{col.subjects}`:`{col.facet2}`) + (1|`{col.facet1}`:`{col.facet2}`)")
   model <- brms::brm(formula = formula1, data = data, family = gaussian(), prior = prior, warmup = warmup,
                iter = iter, chains = chains, cores = cores, threads = brms::threading(2),
                backend = "cmdstanr", control = list(adapt_delta = adapt_delta, max_treedepth = max_treedepth))
